5d68d19b45bd1f40fe08de1d50d49fc96bf0866d
AndiXAM <128737205+AndiXAM@users.noreply.github.com>
2023-12-10 20:39:31 +0300

    Add files via upload

---
+++ guitar/guitar.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var container = document.querySelector('.mainheadercontainer');
+    var animationDuration = 2; // Настройте время анимации здесь (в секундах)
+    container.style.setProperty('--animation-duration', animationDuration + 's');
+    container.classList.add('show');
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const hero = document.querySelector('.hero');
+    
+    function activate(e) {
+      if (e.target.matches('.hero') || !e.target.matches('img')) return;
+      [hero.src, e.target.src] = [e.target.src, hero.src];
+    }
+    
+    window.addEventListener('click', activate, false);
+  });
